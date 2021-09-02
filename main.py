b5c3ea2cc0a629040fd792a2e6fb253e87457164
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-09-02 14:39:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import urllib
 
-path = "/home/kali/Desktop/People/"
+path = "/home/kali/Desktop/Test"
 
 def split(text):
 	lines = text.split("\n")
@@ -11,6 +11,7 @@ def split(text):
 			handle = line.split("|")[3].strip()
 			if handle != "Username":
 				handles.append(handle)
+				print(handle)
 		except IndexError:
 			nope = 0
 	return handles
@@ -18,26 +19,34 @@ def split(text):
 
 for person in os.walk(path):
 	if "handle" in person[2]:
+		print(person[2])
 		file = open(person[0] + "/handle")
 		handle = file.readline()
+		print(handle)
 		try:
 			tagged = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c tagged").read()
 			wtagged = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c wtagged").read()
 			followers = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c followers").read()
 			followings = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c followings").read()
-		except urllib.error.HTTPError:
+		except (urllib.error.HTTPError, instagram_private_api.errors.ClientError, json.decoder.JSONDecodeError) as e:
 			break
 		close = []
 		virtual = []
 		
 		#Tagged List, offline connection
 		taggedList = split(tagged)
-		print(taggedList)
 		wtaggedList = taggedList.extend(split(wtagged))
 		close.append(wtaggedList)
 		#Virtual List, only follow each other on IG, no offline connection
 		followersList = split(followers)
 		followingsList = followersList.extend(split(followings))
 		virtual.append(followingsList)
-			
-		print(close, virtual)
+		os.makedirs(person[0] + "/connections", exist_ok=True)
+		
+		with open(person[0] + "/connections/virtual", "w") as virtualFile:
+			for account in virtual:
+				virtualFile.write(account + "\n")
+		
+		with open(person[0] + "connections/close", "w") as closeFile:
+			for account in close:
+				closeFile.write(account + "\n")
