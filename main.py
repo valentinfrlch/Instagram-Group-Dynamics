d9696a987599ed0c6b82c1c8112ee45b20fce3c5
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-09-02 17:07:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import os
 import urllib
+import json
+import time
+from pathlib import Path
 
-path = "/home/kali/Desktop/Test"
 
-def split(text):
+path = "/home/kali/Desktop/People/"
+tagged = ""
+wtagged = ""
+followers = ""
+followings = ""
+
+
+def extract(text):
 	lines = text.split("\n")
 	handles = []
 	for line in lines:
 		try:
-			handle = line.split("|")[3].strip()
+			handle = line.split("|")[2].strip()
 			if handle != "Username":
 				handles.append(handle)
-				print(handle)
 		except IndexError:
 			nope = 0
 	return handles
 
 
 for person in os.walk(path):
-	if "handle" in person[2]:
-		print(person[2])
+	if ("handle" in person[2]) and (Path(person[0] + '/connections').is_dir() == False):	
 		file = open(person[0] + "/handle")
 		handle = file.readline()
 		print(handle)
@@ -28,25 +35,25 @@ for person in os.walk(path):
 			wtagged = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c wtagged").read()
 			followers = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c followers").read()
 			followings = os.popen("cd /home/kali/tools/Osintgram && python3 main.py " + handle + " -c followings").read()
-		except (urllib.error.HTTPError, instagram_private_api.errors.ClientError, json.decoder.JSONDecodeError) as e:
+		except (urllib.error.HTTPError, json.decoder.JSONDecodeError) as e:
 			break
-		close = []
-		virtual = []
 		
+
+
+
 		#Tagged List, offline connection
-		taggedList = split(tagged)
-		wtaggedList = taggedList.extend(split(wtagged))
-		close.append(wtaggedList)
+		close = extract(tagged) + extract(wtagged)
 		#Virtual List, only follow each other on IG, no offline connection
-		followersList = split(followers)
-		followingsList = followersList.extend(split(followings))
-		virtual.append(followingsList)
+		virtual = extract(followers) + extract(followings)
+		
 		os.makedirs(person[0] + "/connections", exist_ok=True)
+		print(close, virtual)
 		
 		with open(person[0] + "/connections/virtual", "w") as virtualFile:
 			for account in virtual:
 				virtualFile.write(account + "\n")
 		
-		with open(person[0] + "connections/close", "w") as closeFile:
+		with open(person[0] + "/connections/close", "w") as closeFile:
 			for account in close:
 				closeFile.write(account + "\n")
+		time.sleep(15)
